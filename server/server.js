5c37fd00490645bce084320f0d86ae0ecbe1d73b
jescalona <joseaescalonac@gmail.com>
2019-06-14 10:10:20 -0500

    Primer Commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+// el server.js necesita el archivo config.js q se encuentra en la ruta /.config/
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// esta es la definicion de la peticion get
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+
+
+});
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+})
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({ id });
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
